d564b6ed4b5148901176a135aca3ee7185f54993
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2021-05-09 22:00:38 +0200

    Creating Select Header Cell Filter

---
+++ src/app/delinternet-ui/components/table/filters/SelecterCell/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {
+  FilterIcon,
+  IconButton,
+  Position,
+  SelectMenu,
+  Table,
+} from 'evergreen-ui';
+
+const { TextHeaderCell } = Table;
+
+export const SelecterCell = ({
+  title,
+  handler,
+  options = [],
+  selected,
+  icon,
+}) => {
+  const [switchSearch, setSwitchSearch] = React.useState(false);
+
+  const transformOptions = (options = []) => {
+    return options.map(label => ({ label, value: label }));
+  };
+
+  return (
+    <>
+      <TextHeaderCell>{title}</TextHeaderCell>
+      <SelectMenu
+        isShown={switchSearch}
+        position={Position.BOTTOM_RIGHT}
+        title={title}
+        onFilterChange={filter => handler({ filter })}
+        selected={selected}
+        options={transformOptions(options)}
+        onSelect={item => handler({ selected: item.value })}
+        onClose={setSwitchSearch}
+      >
+        <IconButton
+          iconSize={13}
+          appearance="minimal"
+          icon={icon ? icon : FilterIcon}
+        />
+      </SelectMenu>
+    </>
+  );
+};
